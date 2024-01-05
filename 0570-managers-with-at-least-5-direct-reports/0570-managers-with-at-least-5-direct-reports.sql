fccdbf7d1c873e7b1cbea314cd0aed6abeaffd27
Sungwon Lee <82964002+sgwlee96@users.noreply.github.com>
2024-01-04 17:35:51 -0800

    Time: 576 ms (59.40%), Space: 0B (100.00%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,5 +1,14 @@
 # Write your MySQL query statement below
 
+# SELECT t1.name
+#     FROM Employee t1
+#     WHERE ( SELECT COUNT(t2.managerID) as cnt FROM Employee t2 WHERE t1.id = t2.managerID) >= 5;
+
 SELECT t1.name
-    FROM Employee t1
-    WHERE ( SELECT COUNT(t2.managerID) as cnt FROM Employee t2 WHERE t1.id = t2.managerID) >= 5;
+FROM Employee t1
+JOIN (
+    SELECT managerID, COUNT(managerID) AS cnt
+    FROM Employee
+    GROUP BY managerID
+    HAVING COUNT(managerID) >= 5
+) t2 ON t1.id = t2.managerID;
